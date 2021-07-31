f091114839a06e92b6cf0e7b603a85041adcc744
Alireza Ziaee <alrz1999@gmail.com>
2021-07-31 12:03:10 +0430

    CentralEngine module added

---
+++ CentralEngine/src/main/java/ir/sharif/gamin2021/CentralEngine/CentralEngineApplication.java
@@ -0,0 +1,12 @@
+package ir.sharif.gamin2021.CentralEngine;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CentralEngineApplication.class, args);
+    }
+}

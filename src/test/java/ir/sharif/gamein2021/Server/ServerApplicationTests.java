1cf73dd1c0ced1ba72267d00dcd4ed5ab40c3a86
Alireza Ziaee <alrz1999@gmail.com>
2021-07-31 11:28:18 +0430

    root module added

---
+++ src/test/java/ir/sharif/gamein2021/Server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sharif.gamein2021.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

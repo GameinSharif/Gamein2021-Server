e58c179dc1fa495e16dd67f1ee0bd1dbc44957e5
Alireza Ziaee <alrz1999@gmail.com>
2021-09-06 09:40:07 +0430

    modules and exponent added to ConnestionResponse

--- ClientHandler/src/main/java/ir/sharif/gamein2021/ClientHandler/authentication/model/ConnectionResponse.java
+++ ClientHandler/src/main/java/ir/sharif/gamein2021/ClientHandler/authentication/model/ConnectionResponse.java
@@ -8,15 +8,18 @@ import java.security.interfaces.RSAPublicKey;
 
 public class ConnectionResponse extends ResponseObject implements Serializable
 {
-    private byte[] encodedPublicKey;
+    private byte[] exponent;
+    private byte[] modules;
 
     public ConnectionResponse(ResponseTypeConstant responseTypeConstant, RSAPublicKey publicKey)
     {
         this.responseTypeConstant = responseTypeConstant.ordinal();
-        this.encodedPublicKey = publicKey.getEncoded();
+        this.exponent = publicKey.getPublicExponent().toByteArray();
+        this.modules = publicKey.getModulus().toByteArray();
     }
 
-    public ConnectionResponse(byte[] encodedPublicKey) {
-        this.encodedPublicKey = encodedPublicKey;
+    public ConnectionResponse(byte[] exponent, byte[] modules) {
+        this.exponent = exponent;
+        this.modules = modules;
     }
 }

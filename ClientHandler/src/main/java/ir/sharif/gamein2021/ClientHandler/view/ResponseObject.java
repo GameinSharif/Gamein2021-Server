dbad4bda3dc6de3ff78380a8aed4ea04b3eb3519
Shayan Shabanzadeh <shayanshabanzadeh0@gmail.com>
2021-09-16 19:55:49 +0430

    change structure of the project

--- ClientHandler/src/main/java/ir/sharif/gamein2021/ClientHandler/view/ResponseObject.java
+++
@@ -1,8 +0,0 @@
-package ir.sharif.gamein2021.ClientHandler.view;
-
-import java.io.Serializable;
-
-public class ResponseObject implements Serializable
-{
-    public int responseTypeConstant;
-}
--- Migrations/src/main/resources/db/migration/V2.0__Team.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Team
-(
-    id       INT NOT NULL AUTO_INCREMENT,
-    team_name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO Team (team_name)
-values ('team1');

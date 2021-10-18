3f5416a18235a80cca94d091e66c2d8dcbdf4a89
smmhatami <hatamik7@gmail.com>
2021-10-18 15:00:46 +0330

    TransportRepository added

---
+++ Core/src/main/java/ir/sharif/gamein2021/core/dao/TransportRepository.java
@@ -0,0 +1,8 @@
+package ir.sharif.gamein2021.core.dao;
+
+import ir.sharif.gamein2021.core.domain.entity.Transport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransportRepository extends JpaRepository<Transport, Integer> {
+
+}

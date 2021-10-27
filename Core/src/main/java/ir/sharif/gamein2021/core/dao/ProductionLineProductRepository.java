4db6822c4cac082fbe1e31b6749d5e308dcc30a0
Alireza Ziaee <alrz1999@gmail.com>
2021-10-27 23:31:53 +0330

    some part of create product added

--- Core/src/main/java/ir/sharif/gamein2021/core/dao/ProductionLineProductRepository.java
+++ Core/src/main/java/ir/sharif/gamein2021/core/dao/ProductionLineProductRepository.java
@@ -4,4 +4,5 @@ import ir.sharif.gamein2021.core.domain.entity.ProductionLineProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductionLineProductRepository extends JpaRepository<ProductionLineProduct, Integer> {
+    ProductionLineProduct findProductionLineProductByproduc
 }

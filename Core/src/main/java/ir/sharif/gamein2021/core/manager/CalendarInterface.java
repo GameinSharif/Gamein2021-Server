c846a2b6e0d68169d64c1701513e1760ba75b06d
Alireza Ziaee <alrz1999@gmail.com>
2021-10-27 22:41:38 +0330

    refactor

--- Core/src/main/java/ir/sharif/gamein2021/core/manager/CalendarInterface.java
+++
@@ -1,9 +0,0 @@
-package ir.sharif.gamein2021.core.manager;
-
-import java.time.LocalDate;
-
-public interface CalendarInterface {
-    LocalDate getCurrentDate();
-    LocalDate setCurrentDate();
-    void increaseOneDay();
-}

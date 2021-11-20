e747bbdba48cf9ab1c5eed0cb45d30772967da3a
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-11-20 20:38:47 +0330

    fixed a little bug

--- Core/src/main/java/ir/sharif/gamein2021/core/service/ProductionLineProductService.java
+++ Core/src/main/java/ir/sharif/gamein2021/core/service/ProductionLineProductService.java
@@ -6,10 +6,12 @@ import ir.sharif.gamein2021.core.domain.dto.ProductionLineProductDto;
 import ir.sharif.gamein2021.core.domain.entity.ProductionLine;
 import ir.sharif.gamein2021.core.domain.entity.ProductionLineProduct;
 import ir.sharif.gamein2021.core.manager.ReadJsonFilesManager;
+import ir.sharif.gamein2021.core.manager.TeamManager;
 import ir.sharif.gamein2021.core.manager.clientHandlerConnection.ClientHandlerRequestSenderInterface;
 import ir.sharif.gamein2021.core.manager.clientHandlerConnection.requests.ProductCreationCompletedRequest;
 import ir.sharif.gamein2021.core.service.core.AbstractCrudService;
 import ir.sharif.gamein2021.core.util.models.ProductionLineTemplate;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -18,22 +20,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
 @Service
 public class ProductionLineProductService extends AbstractCrudService<ProductionLineProductDto, ProductionLineProduct, Integer> {
     private final ProductionLineProductRepository productionLineProductRepository;
     private final ProductionLineRepository productionLineRepository;
     private final StorageService storageService;
     private final ClientHandlerRequestSenderInterface clientHandlerRequestSender;
-
-    public ProductionLineProductService(ProductionLineProductRepository productionLineProductRepository,
-                                        ProductionLineRepository productionLineRepository,
-                                        StorageService storageService,
-                                        ClientHandlerRequestSenderInterface clientHandlerRequestSender) {
-        this.productionLineProductRepository = productionLineProductRepository;
-        this.productionLineRepository = productionLineRepository;
-        this.storageService = storageService;
-        this.clientHandlerRequestSender = clientHandlerRequestSender;
-    }
+    private final TeamService teamService;
+    private final TeamManager teamManager;
 
     public void finishProductCreation(LocalDate currentDate) {
         List<ProductionLineProduct> products = productionLineProductRepository.findProductionLineProductsByEndDateEquals(currentDate);
@@ -47,15 +42,15 @@ public class ProductionLineProductService extends AbstractCrudService<Production
                 continue;
             }
 
-            // Upgrading brand when production ends.
-            Integer amount = product.getAmount();
-            ProductionLineTemplate productionLineTemplate = ReadJsonFilesManager.ProductionLineTemplates[productionLine.getProductionLineTemplateId()];
-            Float brandCoefficient = (float)(productionLineTemplate.getQualityLevels().get(productionLine.getQualityLevel()).getBrandIncreaseRatioPerProduct());
-            Float newBrand = productionLine.getTeam().getBrand() + amount*brandCoefficient;
-            productionLine.getTeam().setBrand(newBrand);
-
             //TODO efficiency level
-            storageService.addProduct(productionLine.getTeam().getFactoryId(), false, product.getProductId(), product.getAmount());
+            Integer amount = product.getAmount();
+
+            // Upgrading brand when production ends.
+            ProductionLineTemplate productionLineTemplate = ReadJsonFilesManager.ProductionLineTemplateHashMap.getOrDefault(productionLine.getProductionLineTemplateId(), null);
+            Float brandCoefficient = (float)(productionLineTemplate.getQualityLevels().get(productionLine.getQualityLevel()).getBrandIncreaseRatioPerProduct());
+            teamManager.updateTeamBrand(teamService.loadById(productionLine.getTeam().getId()),  amount * brandCoefficient);
+
+            storageService.addProduct(productionLine.getTeam().getFactoryId(), false, product.getProductId(), amount);
             clientHandlerRequestSender.send(new ProductCreationCompletedRequest(productionLine, product, "Done"));
         }
     }

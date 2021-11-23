c10f14168b22d47fe4c81f4fe400cd8721d86256
Alireza Ziaee <alrz1999@gmail.com>
2021-11-23 15:55:45 +0330

    fix brand in weeklyReport

--- Core/src/main/java/ir/sharif/gamein2021/core/service/BusinessIntelligenceService.java
+++ Core/src/main/java/ir/sharif/gamein2021/core/service/BusinessIntelligenceService.java
@@ -5,6 +5,7 @@ import ir.sharif.gamein2021.core.domain.entity.WeeklyReport;
 import ir.sharif.gamein2021.core.manager.GameCalendar;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,19 +16,22 @@ public class BusinessIntelligenceService {
     private final WeeklyReportService weeklyReportService;
     private final TeamService teamService;
     private final GameCalendar gameCalendar;
+    private final Map<Integer, TeamDto> teamsByTeamId;
 
     public BusinessIntelligenceService(WeeklyReportService weeklyReportService, TeamService teamService, GameCalendar gameCalendar) {
         this.weeklyReportService = weeklyReportService;
         this.teamService = teamService;
         this.gameCalendar = gameCalendar;
+
+        List<TeamDto> teams = teamService.getAllTeams();
+        teamsByTeamId = teams.stream().collect(Collectors.toMap(TeamDto::getId, x->x));
     }
 
     public void prepareWeeklyReport() {
-        List<TeamDto> teams = teamService.getAllTeams();
         int weakNumber=  gameCalendar.getCurrentWeek();
-        Map<Integer, WeeklyReport> weeklyReportByTeamId = teams.stream()
+        Map<Integer, WeeklyReport> weeklyReportByTeamId = teamsByTeamId.values().stream()
                 .collect(Collectors.toMap(TeamDto::getId, x -> new WeeklyReport(weakNumber, x.getId())));
-        
+
         setCompetitionReportData(weeklyReportByTeamId);
         setFinanceReportData(weeklyReportByTeamId);
         setCostsReportData(weeklyReportByTeamId);
@@ -46,6 +50,9 @@ public class BusinessIntelligenceService {
     }
 
     private void setCompetitionReportData(Map<Integer, WeeklyReport> weeklyReportByTeamId) {
-        List<TeamDto> teams = teamService.getAllTeams();
+        for (TeamDto team : teamsByTeamId.values()) {
+            WeeklyReport weeklyReport = weeklyReportByTeamId.get(team.getId());
+            weeklyReport.setBrand(team.getBrand());
+        }
     }
 }

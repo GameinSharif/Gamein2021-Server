160aad9cda867a6bdc8180106d6dbfb29210c5c0
Alireza Ziaee <alrz1999@gmail.com>
2021-12-04 09:19:43 +0330

    Bug fix in usedWater

--- Core/src/main/java/ir/sharif/gamein2021/core/service/ProductionLineService.java
+++ Core/src/main/java/ir/sharif/gamein2021/core/service/ProductionLineService.java
@@ -166,9 +166,8 @@ public class ProductionLineService extends AbstractCrudService<ProductionLineDto
 
             for (ProductIngredient productIngredient : productTemplate.getIngredientsPerUnit()) {
                 if (isWater(productIngredient.getProductId())) {
-                    Product water = ReadJsonFilesManager.ProductHashMap.get(4);
                     newCredit -= getWaterCost(amount);
-                    team.setUsedWater(team.getUsedWater() + (long) amount * water.getVolumetricUnit());
+                    team.setUsedWater(team.getUsedWater() + (long) amount * productIngredient.getAmount());
                     continue;
                 }
 

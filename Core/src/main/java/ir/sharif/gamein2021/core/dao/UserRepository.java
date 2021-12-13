3188b7985921f7c96be8816e2f6e524d1bd5df61
Alireza Ziaee <alrz1999@gmail.com>
2021-12-13 16:00:52 +0330

    rename User to Player

--- Core/src/main/java/ir/sharif/gamein2021/core/dao/UserRepository.java
+++ Core/src/main/java/ir/sharif/gamein2021/core/dao/UserRepository.java
@@ -1,12 +1,12 @@
 package ir.sharif.gamein2021.core.dao;
 
-import ir.sharif.gamein2021.core.domain.entity.User;
+import ir.sharif.gamein2021.core.domain.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>
+public interface UserRepository extends JpaRepository<Player, Integer>
 {
-    User findUserByUsernameAndPassword(String username, String password);
-    User findUserById(Integer id);
+    Player findUserByUsernameAndPassword(String username, String password);
+    Player findUserById(Integer id);
 }
--- Core/src/main/java/ir/sharif/gamein2021/core/service/UserService.java
+++ Core/src/main/java/ir/sharif/gamein2021/core/service/UserService.java
@@ -3,13 +3,13 @@ package ir.sharif.gamein2021.core.service;
 import ir.sharif.gamein2021.core.service.core.AbstractCrudService;
 import ir.sharif.gamein2021.core.dao.UserRepository;
 import ir.sharif.gamein2021.core.domain.dto.UserDto;
-import ir.sharif.gamein2021.core.domain.entity.User;
+import ir.sharif.gamein2021.core.domain.entity.Player;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService extends AbstractCrudService<UserDto, User, Integer>
+public class UserService extends AbstractCrudService<UserDto, Player, Integer>
 {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
@@ -24,7 +24,7 @@ public class UserService extends AbstractCrudService<UserDto, User, Integer>
     @Transactional(readOnly = true)
     public UserDto read(String username, String password)
     {
-        User result = userRepository.findUserByUsernameAndPassword(username, password);
+        Player result = userRepository.findUserByUsernameAndPassword(username, password);
         return modelMapper.map(result, UserDto.class);
     }
 

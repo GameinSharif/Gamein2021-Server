e71c3f0c7efefea2cd37db945a764be14c5cd8f4
Alireza Ziaee <alrz1999@gmail.com>
2021-12-14 10:13:43 +0330

    change mysql root password

--- mysql-commands/createUser.sql
+++ mysql-commands/createUser.sql
@@ -1,3 +1,7 @@
 CREATE USER 'readGame'@'%' IDENTIFIED BY 'd454rq23@#$c';
 GRANT SELECT, SHOW VIEW ON gamein_test.* TO 'readGame'@'%';
 FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'eihdea32o3q@%2';
+ALTER USER 'root'@'%' IDENTIFIED BY 'eihdea32o3q@%2';
+FLUSH PRIVILEGES;
